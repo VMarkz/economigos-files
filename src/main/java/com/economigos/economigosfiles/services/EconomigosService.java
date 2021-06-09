a7df6319361a501d9737775b6354e0f5cf21529f
VMarkz <vitor.portela@bandtec.com.br>
2021-06-09 16:44:06 -0300

    now done

--- src/main/java/com/economigos/economigosfiles/services/EconomigosService.java
+++ src/main/java/com/economigos/economigosfiles/services/EconomigosService.java
@@ -20,8 +20,7 @@ public class EconomigosService {
         if (System.getenv("SERVICE_URL") != null){
             baseUrl = System.getenv("SERVICE_URL");
         }else{
-            //baseUrl = "http://ec2-34-236-53-23.compute-1.amazonaws.com:8080";
-            baseUrl = "http://localhost:8080";
+            baseUrl = "http://ec2-34-236-53-23.compute-1.amazonaws.com:8080";
         }
 
         return baseUrl;
